c68d1098e59961a9b0ccb2c33545c6f8a1733d3f
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-14 18:28:22 +0545

    notification added

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,84 +1,121 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import Navbar from "./../homepage/Navbar";
+import { Link } from "react-router-dom";
 
-
-export default function Register() {
+export default function Register(props) {
   return (
-   <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
-    <h2 className="text-2xl font-bold mb-6 text-center">Get started with TodoFlow.</h2>
-    <form>
-      <div className="mb-4">
-        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
-          Username
-        </label>
-        <div className="relative">
-          <FontAwesomeIcon icon={faUser} className="absolute top-3 left-3 text-gray-500" />
-          <input
-            type="text"
-            id="username"
-            name="username"
-            autoComplete='off'
-            className="w-full border border-gray-300 rounded-md py-2 pl-10 focus:outline-none focus:border-blue-500"
-            placeholder="Enter your username"
-          />
+    <>
+      <Navbar />
+      <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
+        <h2 className="text-2xl font-bold mb-6 text-center">
+          Get started with TodoFlow.
+        </h2>
+        <form onSubmit={props.register} name="register">
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="absolute top-3 left-3 text-gray-500"
+              />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                autoComplete="off"
+                className="w-full border border-gray-300 rounded-md py-2 pl-10 focus:outline-none focus:border-blue-500"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <FontAwesomeIcon
+                icon={faLock}
+                className="absolute top-3 left-3 text-gray-500"
+              />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="off"
+                className="w-full border border-gray-300 rounded-md py-2 pl-10 focus:outline-none focus:border-blue-500"
+                placeholder="Enter your password"
+              />
+            </div>
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="cpassword"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Confirm Password
+            </label>
+            <div className="relative">
+              <FontAwesomeIcon
+                icon={faLock}
+                className="absolute top-3 left-3 text-gray-500"
+              />
+              <input
+                type="password"
+                id="cpassword"
+                name="cpassword"
+                autoComplete="off"
+                className="w-full border border-gray-300 rounded-md py-2 pl-10 focus:outline-none focus:border-blue-500"
+                placeholder="Confirm your password"
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+          >
+            Register
+          </button>
+        </form>
+
+        <p className="text-center mt-4">Or register with</p>
+        <div>
+          <button className="w-full mt-2 bg-cyan-400 text-white py-2 rounded-md hover:bg-cyan-600 transition duration-300">
+            <FontAwesomeIcon
+              icon={faGoogle}
+              size="lg"
+              className="text-yellow-400 pr-2"
+            />
+            <span>Google</span>
+          </button>
+        </div>
+        <div className="text-center my-3">
+          <p className="font-light text-gray-600">
+            Already have an account yet?{" "}
+            <Link
+              to="/login"
+              className="font-medium text-primary-600 hover:underline"
+            >
+              Sign In
+            </Link>
+          </p>
         </div>
       </div>
-
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-          Email
-        </label>
-        <div className="relative">
-          <FontAwesomeIcon icon={faEnvelope} className="absolute top-3 left-3 text-gray-500" />
-          <input
-            type="email"
-            id="email"
-            name="email"
-            autoComplete='off'
-            className="w-full border border-gray-300 rounded-md py-2 pl-10 focus:outline-none focus:border-blue-500"
-            placeholder="Enter your email"
-          />
-        </div>
-      </div>
-
-      <div className="mb-6">
-        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-          Password
-        </label>
-        <div className="relative">
-          <FontAwesomeIcon icon={faLock} className="absolute top-3 left-3 text-gray-500" />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            autoComplete='off'
-            className="w-full border border-gray-300 rounded-md py-2 pl-10 focus:outline-none focus:border-blue-500"
-            placeholder="Enter your password"
-          />
-        </div>
-      </div>
-
-      <button
-        type="submit"
-        className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
-      >
-        Register
-      </button>
-    </form>
-
-    <p className="text-center mt-4">Or register with</p>
-    <div>
-    <button
-      className="w-full mt-2 bg-cyan-400 text-white py-2 rounded-md hover:bg-cyan-600 transition duration-300"
-    >
-      <FontAwesomeIcon icon={faGoogle} size="lg" className='text-yellow-400 pr-2' />
-      <span>Google</span>
-    </button>
-    </div>
-  </div>
-  )
+    </>
+  );
 }
--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,39 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function Home() {
   return (
     <>
-    <div className="relative bg-gray-900 min-h-screen">
-      <div className="container mx-auto px-6 pt-36">
-        <div className="lg:w-2/3 text-center mx-auto">
-          <h1 className="text-white font-bold text-5xl md:text-6xl xl:text-7xl">
-            Transform your day with {""}
-            <span className="text-white">TodoFlow</span>
-          </h1>
-          <p className="mt-8 text-xl text-gray-300">
-            Discover a world of efficiency, unlock your full potential, and
-            redefine how you conquer your tasks.
-          </p>
-          <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
-            <Link
-              to="/register"
-              className="relative inline-block px-8 py-4 bg-green-500 text-white rounded-full font-semibold text-lg hover:bg-opacity-90 transition duration-300"
-            >
-              Get started
-            </Link>
-            <Link
-              to="/home"
-              className="relative inline-block px-8 py-4 border border-green-500 text-green-500 rounded-full font-semibold text-lg hover:bg-green-500 hover:text-white transition duration-300"
-            >
-              Learn more
-            </Link>
+      <Navbar />
+      <div className="relative bg-gray-900 min-h-screen">
+        <div className="container mx-auto px-6 pt-36">
+          <div className="lg:w-2/3 text-center mx-auto">
+            <h1 className="text-white font-bold text-5xl md:text-6xl xl:text-7xl">
+              Transform your day with {""}
+              <span className="text-white">TodoFlow</span>
+            </h1>
+            <p className="mt-8 text-xl text-gray-300">
+              Discover a world of efficiency, unlock your full potential, and
+              redefine how you conquer your tasks.
+            </p>
+            <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+              <Link
+                to="/register"
+                className="relative inline-block px-8 py-4 bg-green-500 text-white rounded-full font-semibold text-lg hover:bg-opacity-90 transition duration-300"
+              >
+                Get started
+              </Link>
+              <Link
+                to="/home"
+                className="relative inline-block px-8 py-4 border border-green-500 text-green-500 rounded-full font-semibold text-lg hover:bg-green-500 hover:text-white transition duration-300"
+              >
+                Learn more
+              </Link>
+            </div>
+            {/* Add other sections or components as needed */}
           </div>
-          {/* Add other sections or components as needed */}
         </div>
+        {/* Footer - Add your footer component here */}
       </div>
-      {/* Footer - Add your footer component here */}
-    </div>
     </>
   );
 }
