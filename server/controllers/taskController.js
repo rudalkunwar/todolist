48551d51d68be3f1c8e7124ad6613cc0cb5d470c
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-02-17 20:34:00 +0545

    delete controller made

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -2,11 +2,9 @@ const Task = require("../models/task");
 
 const get_task = async (req, res) => {
   const user = req.params.user;
-
   try {
     // Find tasks associated with the user's username
     const tasks = await Task.find({ user });
-
     if (tasks.length === 0) {
       return res.status(200).json({ message: "No tasks found for the user" });
     }
@@ -17,38 +15,38 @@ const get_task = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
-module.exports = get_task;
-
 const add_task = async (req, res) => {
   try {
     const { title, description, user } = req.body;
-    console.log(req.body);
-    // Create a new task using Task model
     const task = await Task.create({ title, description, user });
-    console.log(task);
-    // Check if task was successfully created
     if (task) {
       res.status(200).json({ message: "Task Added Successfully", task });
     } else {
       res.status(500).json({ message: "Failed to add Task" });
     }
   } catch (error) {
-    console.error("Error adding task:", error);
     res
       .status(500)
       .json({ message: "Failed to add Task", error: error.message });
   }
 };
-
-const edit_task = (req, res) => {};
-const update_task = (req, res) => {};
-const delete_task = (req, res) => {};
+const delete_task = async (req, res) => {
+  const id = req.query.id;
+  console.log(id);
+  try {
+    const response = await Task.findByIdAndDelete(id);
+    if (response) {
+      res.status(200).json({ message: "Task Deleted Successfully" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to delete Task", error: error.message });
+  }
+};
 
 module.exports = {
   get_task,
   add_task,
-  edit_task,
-  update_task,
   delete_task,
 };
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -5,8 +5,6 @@ const router = Router();
 
 router.get("/todolist/:user", taskController.get_task);
 router.post("/task/add/", taskController.add_task);
-router.get("/task/edit/:id", taskController.add_task);
-router.put("/task/update/:id", taskController.add_task);
-router.delete("/task/delete/:id", taskController.add_task);
+router.delete("/task/delete/", taskController.delete_task);
 
 module.exports = router;
